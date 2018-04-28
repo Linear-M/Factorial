b569c978aa25c18252f46e24c8c45e6a41ef57d7
Ben Pople <masterbp@hotmail.co.uk>
2018-04-28 16:41:20 +0100

    Add project files.

---
+++ Factorial/Factorial-DESKTOP-VAP1626.cpp
@@ -0,0 +1,69 @@
+#include "stdafx.h"
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+#include <limits>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
+unsigned long long multiply(long x, long res[], long res_size);
+void factorial(long n);
+const int max = 1500000;
+
+int main() {
+	factorial(150000);
+	return 0;
+}
+
+void factorial(long n)
+{
+	long overflow[max];
+	//Instantiate the array which will hold overflows of the main factorial number
+	overflow[0] = 1;
+	long overflow_size = 1;
+
+	//Iterative factorial approach (calls function multiply however is still loop-based)
+	for (long x = 1; x <= n; x++){
+		overflow_size = multiply(x, overflow, overflow_size);
+	}
+
+	//Create output information
+	ofstream outputFile;
+	outputFile.open("C:\\Ben\\Factorial - " + to_string(n) + ".txt");
+
+	//Print the results of the loop
+	for (long i = overflow_size - 1; i >= 0; i--){
+		outputFile << overflow[i];
+	}
+
+	outputFile.close();
+}
+
+// Takes res[] as input (representation/number of digits) and multiplies it by the long x
+unsigned long long multiply(long x, long res[], long res_size)
+{
+	/*Starting some GCSE maths*/
+	long carry = 0;  
+
+	// For each individual integer multiply by x
+	for (long i = 0; i<res_size; i++)
+	{
+		long prod = res[i] * x + carry;
+		/*New last digit*/
+		res[i] = prod % 10;
+		/*Overflow stored by carry*/
+		carry = prod / 10;
+	}
+
+	/*Move carry to the array and free buffers*/
+	while (carry)
+	{
+		res[res_size] = carry % 10;
+		carry = carry / 10;
+		res_size++;
+	}
+	return res_size;
+}
